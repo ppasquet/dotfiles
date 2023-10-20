13c2e9c43457b8c64c97a5f2d0ccf27777df58b5
Pierre Pasquet <80340281+ppasquet@users.noreply.github.com>
2023-10-20 09:52:50 -0400

    upgrading telescope

--- nvim/lua/pp492/packer.lua
+++ nvim/lua/pp492/packer.lua
@@ -1,8 +1,7 @@
 return require('packer').startup(function(use)
 	use 'wbthomason/packer.nvim'
 	use {
-		'nvim-telescope/telescope.nvim', tag = '0.1.0',
-		-- or                            , branch = '0.1.x',
+		'nvim-telescope/telescope.nvim', tag = '0.1.4',
 		requires = { {'nvim-lua/plenary.nvim'} }
 	}
     use { "ellisonleao/gruvbox.nvim" }
