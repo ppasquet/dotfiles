fdf0493e0d1938736cdd004a46fdd01a938310b0
Pierre Pasquet <pasquet.pierre@icloud.com>
2023-11-09 22:46:26 -0500

    new LSP config

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -1,23 +1,113 @@
-local lsp = require("lsp-zero")
+local lspconfig = require("lspconfig")
+local lsp_capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-lsp.preset("recommended")
+vim.api.nvim_create_autocmd('LspAttach', {
+  desc = 'LSP actions',
+  callback = function(event)
+    local opts = {buffer = event.buf}
+
+	-- vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+	-- vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+	-- vim.keymap.set("n", "<leader>vws", vim.lsp.buf.workspace_symbol, opts)
+	-- vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float, opts)
+	-- vim.keymap.set("n", "[d", vim.diagnostic.goto_next, opts)
+	-- vim.keymap.set("n", "]d", vim.diagnostic.goto_prev, opts)
+	-- vim.keymap.set("n", "<leader>vca", vim.lsp.buf.code_action, opts)
+	-- vim.keymap.set("n", "<leader>vrr", vim.lsp.buf.references, opts)
+	-- vim.keymap.set("n", "<leader>vrn", vim.lsp.buf.rename, opts)
+ --    vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, opts)
+	-- vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, opts)
+
+    vim.keymap.set('n', 'K', "<cmd>lua vim.lsp.buf.hover()<cr>", opts)
+    vim.keymap.set('n', "gd", '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+    vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+    vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+    vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
+    vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
+    vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+    vim.keymap.set('n', "<leader>vrn", '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+    vim.keymap.set({'n', 'x'}, "<leader>f", '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
+    vim.keymap.set('n', "<leader>vca", '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+
+    vim.keymap.set('n', "<leader>vd", "<cmd>lua vim.diagnostic.open_float()<cr>", opts)
+    vim.keymap.set('n', "[d", "<cmd>lua vim.diagnostic.goto_prev()<cr>", opts)
+    vim.keymap.set('n', "]d", "<cmd>lua vim.diagnostic.goto_next()<cr>", opts) 
+  end
+})
+
+local default_setup = function(server)
+  lspconfig[server].setup({
+    capabilities = lsp_capabilities,
+  })
+end
+
+require('mason').setup({})
+require('mason-lspconfig').setup({
+    ensure_installed = {},
+    handlers = {
+        default_setup,
+        lua_ls = function()
+            require("lspconfig").lua_ls.setup({
+                settings = {
+                    Lua = {
+                        runtime = {
+                            version = "LuaJIT"
+                        },
+                        diagnostics = {
+                            globals = {"vim"},
+                        },
+                        workspace = {
+                            library = {
+                                vim.env.VIMRUNTIME,
+                            }
+                        }
+                    }
+                }
+            })
+        end,
+    },
+})
+
+local cmp = require('cmp')
+
+cmp.setup({
+  sources = {
+    {name = "nvim_lsp"},
+  },
+  mapping = cmp.mapping.preset.insert({
+    -- Enter key confirms completion item
+    ["<CR>"] = cmp.mapping.confirm({select = false}),
+	["<C-p>"] = cmp.mapping.select_prev_item({behavior = cmp.SelectBehavior.Select}),
+	['<C-n>'] = cmp.mapping.select_next_item({behavior = cmp.SelectBehavior.Select}),
+    -- Ctrl + space triggers completion menu
+    ["<C-Space>"] = cmp.mapping.complete(),
+  }),
+  snippet = {
+    expand = function(args)
+      require("luasnip").lsp_expand(args.body)
+    end,
+  },
+})
+-- local lsp = require("lsp-zero")
+--
+-- lsp.preset("recommended")
 
 -- Fix Undefined global 'vim'
-lsp.configure('lua_ls', {
-	settings = {
-		Lua = {
-            runtime = {
-                version = "LuaJIT",
-            },
-			diagnostics = {
-				globals = { 'vim' }
-			},
-            telemetry = {
-                enable = false
-            }
-		}
-	}
-})
+-- lsp.configure('lua_ls', {
+-- 	settings = {
+-- 		Lua = {
+--             runtime = {
+--                 version = "LuaJIT",
+--             },
+-- 			diagnostics = {
+-- 				globals = { 'vim' }
+-- 			},
+--             telemetry = {
+--                 enable = false
+--             }
+-- 		}
+-- 	}
+-- })
 
 -- local cmp = require('cmp')
 -- local cmp_select = {behavior = cmp.SelectBehavior.Select}
@@ -32,35 +122,35 @@ lsp.configure('lua_ls', {
 -- 	mapping = cmp_mappings
 -- })
 
-lsp.set_preferences({
-	suggest_lsp_servers = false,
-	sign_icons = {
-		error = 'E',
-		warn = 'W',
-		hint = 'H',
-		info = 'I'
-	}
-})
-
-lsp.on_attach(function(_, bufnr)
-	local opts = {buffer = bufnr, remap = false}
-
-	vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-	vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-	vim.keymap.set("n", "<leader>vws", vim.lsp.buf.workspace_symbol, opts)
-	vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float, opts)
-	vim.keymap.set("n", "[d", vim.diagnostic.goto_next, opts)
-	vim.keymap.set("n", "]d", vim.diagnostic.goto_prev, opts)
-	vim.keymap.set("n", "<leader>vca", vim.lsp.buf.code_action, opts)
-	vim.keymap.set("n", "<leader>vrr", vim.lsp.buf.references, opts)
-	vim.keymap.set("n", "<leader>vrn", vim.lsp.buf.rename, opts)
-    vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, opts)
-	vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, opts)
-end)
-
-lsp.setup()
-
-vim.diagnostic.config({
-	virtual_text = true,
-})
-
+-- lsp.set_preferences({
+-- 	suggest_lsp_servers = false,
+-- 	sign_icons = {
+-- 		error = 'E',
+-- 		warn = 'W',
+-- 		hint = 'H',
+-- 		info = 'I'
+-- 	}
+-- })
+--
+-- lsp.on_attach(function(_, bufnr)
+-- 	local opts = {buffer = bufnr, remap = false}
+--
+	-- vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+	-- vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+	-- vim.keymap.set("n", "<leader>vws", vim.lsp.buf.workspace_symbol, opts)
+	-- vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float, opts)
+	-- vim.keymap.set("n", "[d", vim.diagnostic.goto_next, opts)
+	-- vim.keymap.set("n", "]d", vim.diagnostic.goto_prev, opts)
+	-- vim.keymap.set("n", "<leader>vca", vim.lsp.buf.code_action, opts)
+	-- vim.keymap.set("n", "<leader>vrr", vim.lsp.buf.references, opts)
+	-- vim.keymap.set("n", "<leader>vrn", vim.lsp.buf.rename, opts)
+ --    vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, opts)
+	-- vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, opts)
+-- end)
+--
+-- lsp.setup()
+--
+-- vim.diagnostic.config({
+-- 	virtual_text = true,
+-- })
+--
--- nvim/lua/pp492/packer.lua
+++ nvim/lua/pp492/packer.lua
@@ -6,28 +6,34 @@ return require('packer').startup(function(use)
 	}
     use { "ellisonleao/gruvbox.nvim" }
 	use ('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
-	use {
-		'VonHeikemen/lsp-zero.nvim',
-		requires = {
-			-- LSP Support
-			{'neovim/nvim-lspconfig'},
-			{'williamboman/mason.nvim'},
-			{'williamboman/mason-lspconfig.nvim'},
-
-			-- Autocompletion
-			{'hrsh7th/nvim-cmp'},
-			{'hrsh7th/cmp-buffer'},
-			{'hrsh7th/cmp-path'},
-			{'saadparwaiz1/cmp_luasnip'},
-			{'hrsh7th/cmp-nvim-lsp'},
-			{'hrsh7th/cmp-nvim-lua'},
-
-			-- Snippets
-			{'L3MON4D3/LuaSnip'},
-			{'rafamadriz/friendly-snippets'},
-
-		}
-	}
+    use { "neovim/nvim-lspconfig" }
+    use { "williamboman/mason.nvim" }
+    use { "williamboman/mason-lspconfig.nvim" }
+    use { "hrsh7th/nvim-cmp" }
+    use { "hrsh7th/cmp-nvim-lsp" }
+    use { "L3MON4D3/LuaSnip" }
+	-- use {
+	-- 	'VonHeikemen/lsp-zero.nvim',
+	-- 	requires = {
+	-- 		-- LSP Support
+	-- 		{'neovim/nvim-lspconfig'},
+	-- 		{'williamboman/mason.nvim'},
+	-- 		{'williamboman/mason-lspconfig.nvim'},
+	--
+	-- 		-- Autocompletion
+	-- 		{'hrsh7th/nvim-cmp'},
+	-- 		{'hrsh7th/cmp-buffer'},
+	-- 		{'hrsh7th/cmp-path'},
+	-- 		{'saadparwaiz1/cmp_luasnip'},
+	-- 		{'hrsh7th/cmp-nvim-lsp'},
+	-- 		{'hrsh7th/cmp-nvim-lua'},
+	--
+	-- 		-- Snippets
+	-- 		{'L3MON4D3/LuaSnip'},
+	-- 		{'rafamadriz/friendly-snippets'},
+	--
+	-- 	}
+	-- }
     use { "tpope/vim-surround" }
     use { 
         "numToStr/Comment.nvim", 
