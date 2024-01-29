02f2c03b27bf8a69bc6a0a7533e7c877582653c0
Pierre Pasquet <pierre@paces.com>
2024-01-28 23:11:12 -0500

    woop

--- .zshenv
+++ .zshenv
@@ -1,4 +1,3 @@
 export DYLD_LIBRARY_PATH=/opt/homebrew/opt/geos/lib/
 export SPATIALINDEX_C_LIBRARY=/opt/homebrew/Cellar/spatialindex/1.9.3/lib
 export PYENV_ROOT="$HOME/.pyenv"
-[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
--- nvim/after/plugin/obsidian.lua
+++ nvim/after/plugin/obsidian.lua
@@ -17,4 +17,24 @@ require("obsidian").setup({
         -- vim.fn.jobstart({"xdg-open", url})  -- linux
     end,
     open_notes_in = "vsplit",
+    -- note_id_func = function(title)
+    --     -- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
+    --     -- In this case a note with the title 'My new note' will be given an ID that looks
+    --     -- like '1657296016-my-new-note', and therefore the file name '1657296016-my-new-note.md'
+    --     local suffix = ""
+    --     if title ~= nil then
+    --       -- If title is given, transform it into valid file name.
+    --       suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
+    --     else
+    --       -- If title is nil, just add 4 random uppercase letters to the suffix.
+    --       for _ = 1, 4 do
+    --         suffix = suffix .. string.char(math.random(65, 90))
+    --       end
+    --     end
+    --     return tostring(os.time()) .. "-" .. suffix
+    -- end,
+    note_id_func = function(title) 
+        return title
+    end,
+
 })
