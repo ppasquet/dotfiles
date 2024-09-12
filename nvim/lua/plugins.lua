513e99942a43faf2e3b448c92760c46bbe987479
Pierre Pasquet <pierre@paces.com>
2024-09-12 17:37:15 -0400

    yeet

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -22,4 +22,30 @@ return {
     { "tpope/vim-fugitive" },
     { "numToStr/Comment.nvim" },
     { "lewis6991/gitsigns.nvim" },
+    { 
+        "mfussenegger/nvim-dap",
+        dependencies = {
+            "rcarriga/nvim-dap-ui",
+            "theHamsta/nvim-dap-virtual-text",
+            "nvim-neotest/nvim-nio",
+            "williamboman/mason.nvim"
+        },
+        config = function()
+            local dap = require "dap"
+            local ui = require "dapui"
+
+            require("dapui").setup()
+            vim.keymap.set("n", "<space>b", dap.toggle_breakpoint)
+            vim.keymap.set("n", "<space>?", function()
+                require("dapui").eval(nil, { enter = true })
+            end)
+
+            vim.keymap.set("n", "<F1>", dap.continue)
+            vim.keymap.set("n", "<F2>", dap.step_into)
+            vim.keymap.set("n", "<F3>", dap.step_over)
+            vim.keymap.set("n", "<F4>", dap.step_out)
+            vim.keymap.set("n", "<F5>", dap.step_back)
+            vim.keymap.set("n", "<F13>", dap.restart)
+        end
+    }
 }
