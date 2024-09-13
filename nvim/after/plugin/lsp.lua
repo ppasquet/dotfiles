1eceaf3d5b8217432e076adbbc6dda375590aed9
Pierre Pasquet <pasquet.pierre@icloud.com>
2024-09-12 23:33:24 -0400

    python lsp

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -1,84 +1,81 @@
-local lspconfig = require("lspconfig")
-local lsp_capabilities = require("cmp_nvim_lsp").default_capabilities()
-
-vim.api.nvim_create_autocmd('LspAttach', {
-  desc = 'LSP actions',
-  callback = function(event)
-    local opts = {buffer = event.buf}
-
-    vim.keymap.set('n', 'K', "<cmd>lua vim.lsp.buf.hover()<cr>", opts)
-    -- vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
-    -- vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
-    -- vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
-    vim.keymap.set('n', "<leader>vrn", '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
-    -- vim.keymap.set({'n', 'x'}, "<leader>f", '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
-    vim.keymap.set('n', "<leader>vca", '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
-
-    vim.keymap.set('n', "<leader>vd", "<cmd>lua vim.diagnostic.open_float()<cr>", opts)
-    -- vim.keymap.set('n', "[d", "<cmd>lua vim.diagnostic.goto_prev()<cr>", opts)
-    -- vim.keymap.set('n', "]d", "<cmd>lua vim.diagnostic.goto_next()<cr>", opts) 
-  end
-})
-
-local default_setup = function(server)
-  lspconfig[server].setup({
-    capabilities = lsp_capabilities,
-  })
-end
-
-require('mason').setup({})
-require('mason-lspconfig').setup({
-    ensure_installed = {},
-    handlers = {
-        default_setup,
-        lua_ls = function()
-            require("lspconfig").lua_ls.setup({
-                settings = {
-                    Lua = {
-                        runtime = {
-                            version = "LuaJIT"
-                        },
-                        diagnostics = {
-                            globals = {"vim"},
-                        },
-                        workspace = {
-                            library = {
-                                vim.env.VIMRUNTIME,
-                            }
-                        }
-                    }
-                }
-            })
-        end,
+require "lspconfig".pyright.setup{
+    cmd = {
+        "/Users/pierrepasquet/.venv/bin/pyright-langserver", "--stdio"
+    }, 
+    settings = {
+        pyright = {
+            disableOrganizeInports = true,
+        },
+        python = {
+            analysis = {
+                ignore = { "*" }
+            }
+        }
     },
-})
-require('mason-lspconfig').setup_handlers({
-    function(server_name)
-        if server_name == "tsserver" then
-            server_name = "ts_ls"
+    filetypes = { "python" },
+}
+
+require "lspconfig".ruff.setup{
+    cmd = { "ruff", "server" }, 
+    filetypes = { "python" },
+}
+
+vim.api.nvim_create_autocmd("LspAttach", {
+    group = vim.api.nvim_create_augroup("lsp_attach_disable_ruff_hover", { clear = true }),
+    callback = function(args)
+        local client = vim.lsp.get_client_by_id(args.data.client_id)
+        if client == nil then
+            return
         end
-        local capabilities = require("cmp_nvim_lsp").default_capabilities()
-        require("lspconfig")[server_name].setup({capabilities=capabilities})
+        if client.name == "ruff" then
+            client.server_capabilities.hoverProvider = false
+        end
+        local opts = { buffer = args.buf }
+        vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+        vim.keymap.set('n', "<leader>vrn", vim.lsp.buf.rename, opts)
+        vim.keymap.set('n', "<leader>vd", vim.diagnostic.open_float, opts)
+        vim.keymap.set('n', "<leader>vca", vim.lsp.buf.code_action, opts)
+        vim.keymap.set('n', "<leader>f", vim.lsp.buf.format, opts)
     end
 })
 
-local cmp = require('cmp')
+-- vim.api.nvim_create_autocmd('LspAttach', {
+--   desc = 'LSP actions',
+--   callback = function(event)
+--     local opts = {buffer = event.buf}
+--
+--     vim.keymap.set('n', 'K', "<cmd>lua vim.lsp.buf.hover()<cr>", opts)
+--     -- vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+--     -- vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
+--     -- vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+--     vim.keymap.set('n', "<leader>vrn", '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+--     -- vim.keymap.set({'n', 'x'}, "<leader>f", '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
+--     vim.keymap.set('n', "<leader>vca", '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+--
+--     vim.keymap.set('n', "<leader>vd", "<cmd>lua vim.diagnostic.open_float()<cr>", opts)
+--     -- vim.keymap.set('n', "[d", "<cmd>lua vim.diagnostic.goto_prev()<cr>", opts)
+--     -- vim.keymap.set('n', "]d", "<cmd>lua vim.diagnostic.goto_next()<cr>", opts) 
+--   end
+-- })
 
-cmp.setup({
-  sources = {
-    {name = "nvim_lsp"},
-  },
-  mapping = cmp.mapping.preset.insert({
-    -- Enter key confirms completion item
-    ["<CR>"] = cmp.mapping.confirm({select = false}),
-	["<C-p>"] = cmp.mapping.select_prev_item({behavior = cmp.SelectBehavior.Select}),
-	['<C-n>'] = cmp.mapping.select_next_item({behavior = cmp.SelectBehavior.Select}),
-    -- Ctrl + space triggers completion menu
-    ["<C-Space>"] = cmp.mapping.complete(),
-  }),
-  snippet = {
-    expand = function(args)
-      require("luasnip").lsp_expand(args.body)
-    end,
-  },
-})
+
+-- local cmp = require('cmp')
+--
+-- cmp.setup({
+--   sources = {
+--     {name = "nvim_lsp"},
+--   },
+--   mapping = cmp.mapping.preset.insert({
+--     -- Enter key confirms completion item
+--     ["<CR>"] = cmp.mapping.confirm({select = false}),
+-- 	["<C-p>"] = cmp.mapping.select_prev_item({behavior = cmp.SelectBehavior.Select}),
+-- 	['<C-n>'] = cmp.mapping.select_next_item({behavior = cmp.SelectBehavior.Select}),
+--     -- Ctrl + space triggers completion menu
+--     ["<C-Space>"] = cmp.mapping.complete(),kk
+--   }),
+--   snippet = {
+--     expand = function(args)
+--       require("luasnip").lsp_expand(args.body)
+--     end,
+--   },
+-- })
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -5,47 +5,31 @@ return {
             "nvim-lua/plenary.nvim"
         }
     },
-    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-    { "ellisonleao/gruvbox.nvim", priority = 1000, config = true },
-    { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
     { 
-        "williamboman/mason.nvim",
-        dependencies = {
-            "williamboman/mason-lspconfig.nvim",
-            "neovim/nvim-lspconfig",
-            "hrsh7th/nvim-cmp"
-        }
+        "nvim-telescope/telescope-fzf-native.nvim", 
+        build = "make" 
     },
-    { "hrsh7th/cmp-nvim-lsp" },
+    { 
+        "ellisonleao/gruvbox.nvim", 
+        priority = 1000, config = true 
+    },
+    { 
+        "nvim-treesitter/nvim-treesitter", 
+        build = ":TSUpdate" 
+    },
+    { 
+        "neovim/nvim-lspconfig",
+    },
+    -- {
+    --     "hrsh7th/cmp-nvim-lsp"
+    -- },
+    -- {
+    --     "hrsh7th/nvim-cmp"
+    -- }
+    -- { "hrsh7th/cmp-nvim-lsp" },
     { "tpope/vim-surround" },
     { "tpope/vim-obsession" },
     { "tpope/vim-fugitive" },
     { "numToStr/Comment.nvim" },
     { "lewis6991/gitsigns.nvim" },
-    { 
-        "mfussenegger/nvim-dap",
-        dependencies = {
-            "rcarriga/nvim-dap-ui",
-            "theHamsta/nvim-dap-virtual-text",
-            "nvim-neotest/nvim-nio",
-            "williamboman/mason.nvim"
-        },
-        config = function()
-            local dap = require "dap"
-            local ui = require "dapui"
-
-            require("dapui").setup()
-            vim.keymap.set("n", "<space>b", dap.toggle_breakpoint)
-            vim.keymap.set("n", "<space>?", function()
-                require("dapui").eval(nil, { enter = true })
-            end)
-
-            vim.keymap.set("n", "<F1>", dap.continue)
-            vim.keymap.set("n", "<F2>", dap.step_into)
-            vim.keymap.set("n", "<F3>", dap.step_over)
-            vim.keymap.set("n", "<F4>", dap.step_out)
-            vim.keymap.set("n", "<F5>", dap.step_back)
-            vim.keymap.set("n", "<F13>", dap.restart)
-        end
-    }
 }
