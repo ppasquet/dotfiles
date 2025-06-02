ae01b728d933957fd5c0e2240dd990e9094a23ed
Pierre Pasquet <pasquet.pierre@icloud.com>
2025-06-01 21:51:43 -0400

    air config

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,15 +1,3 @@
 require("config.lazy")
 require("config.remap")
 require("config.set")
-
-vim.lsp.config('rust_analyzer', {
-  settings = {
-    ['rust-analyzer'] = {
-      diagnostics = {
-        enable = false;
-      }
-    }
-  }
-})
-
-vim.lsp.enable("rust_analyzer")
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,19 +1,80 @@
 return {
-    -- { 
-    --     "nvim-telescope/telescope.nvim", tag = "0.1.8",
-    --     dependencies = {
-    --         "nvim-lua/plenary.nvim"
-    --     }
-    -- },
-    -- { 
-    --     "nvim-telescope/telescope-fzf-native.nvim", 
-    --     build = "make" 
-    -- },
+    {
+        "nvim-telescope/telescope.nvim",
+        tag = "0.1.8",
+        dependencies = {
+            "nvim-lua/plenary.nvim"
+        }
+    },
+    {
+        "nvim-telescope/telescope-fzf-native.nvim",
+        build = "make"
+    },
     {
         "ellisonleao/gruvbox.nvim",
-        priority = 1000, config = true
+        priority = 1000,
+        config = true
     },
-    { "neovim/nvim-lspconfig" },
+    {
+        'saghen/blink.cmp',
+        dependencies = 'rafamadriz/friendly-snippets',
+
+        version = 'v0.*',
+
+        opts = {
+            keymap = { preset = 'default' },
+
+            appearance = {
+                use_nvim_cmp_as_default = true,
+                -- nerd_font_variant = 'mono'
+            },
+
+            signature = { enabled = true },
+            fuzzy = { implementation = "prefer_rust_with_warning" }
+        },
+    },
+    {
+        "neovim/nvim-lspconfig",
+        dependencies = {
+            'saghen/blink.cmp',
+            {
+                "folke/lazydev.nvim",
+                opts = {
+                    library = {
+                        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+                    },
+                },
+            },
+        },
+        config = function()
+            local capabilities = require('blink.cmp').get_lsp_capabilities()
+            local lspconfig = require("lspconfig")
+            lspconfig["lua_ls"].setup({ capabilities = capabilities })
+            lspconfig["rust_analyzer"].setup({ capabilities = capabilities })
+
+            vim.api.nvim_create_autocmd('LspAttach', {
+                callback = function(args)
+                    local c = vim.lsp.get_client_by_id(args.data.client_id)
+                    if not c then return end
+                    local opts = { buffer = args.buf }
+                    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+                    vim.keymap.set('n', "<leader>vrn", vim.lsp.buf.rename, opts)
+                    vim.keymap.set('n', "<leader>vd", vim.diagnostic.open_float, opts)
+                    vim.keymap.set('n', "<leader>vca", vim.lsp.buf.code_action, opts)
+                    vim.keymap.set('n', "<leader>f", vim.lsp.buf.format, opts)
+                    if vim.bo.filetype == "lua" then
+                        -- Format the current buffer on save
+                        vim.api.nvim_create_autocmd('BufWritePre', {
+                            buffer = args.buf,
+                            callback = function()
+                                vim.lsp.buf.format({ bufnr = args.buf, id = c.id })
+                            end,
+                        })
+                    end
+                end,
+            })
+        end,
+    },
     {
         "nvim-treesitter/nvim-treesitter",
         build = ":TSUpdate",
@@ -25,24 +86,27 @@ return {
                 },
                 sync_install = false,
                 auto_install = true,
-                highlight = { enable = true },
+                highlight = {
+                    enable = true,
+                    disable = function(_, buf)
+                        local max_filesize = 100 * 1024 -- 100 KB
+                        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+                        if ok and stats and stats.size > max_filesize then
+                            return true
+                        end
+                    end,
+                    additional_vim_regex_highlighting = false,
+                },
                 additional_vim_regex_highlighting = false
             })
         end
     },
-    -- { "hrsh7th/cmp-nvim-lsp" },
-    -- { "hrsh7th/cmp-buffer" },
-    -- { "hrsh7th/cmp-path" },
-    -- { "hrsh7th/cmp-cmdline" },
-    -- { "hrsh7th/nvim-cmp" },
-    -- { "L3MON4D3/LuaSnip" },
-    -- { "saadparwaiz1/cmp_luasnip" },
-    -- { "tpope/vim-surround" },
-    -- { "tpope/vim-obsession" },
-    -- { "tpope/vim-fugitive" },
-    -- { "numToStr/Comment.nvim" },
-    -- { "lewis6991/gitsigns.nvim" },
-    -- {
-    --     "ruifm/gitlinker.nvim"
-    -- }
+    { "tpope/vim-surround" },
+    { "tpope/vim-obsession" },
+    { "tpope/vim-fugitive" },
+    { "numToStr/Comment.nvim" },
+    { "lewis6991/gitsigns.nvim" },
+    {
+        "ruifm/gitlinker.nvim"
+    }
 }
