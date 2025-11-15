b926b4ade1ded7de52e99dcafc5c346f5e7ee7c0
Pierre Pasquet <pasquet.pierre@icloud.com>
2025-11-15 17:23:30 -0500

    new

--- nvim/lsp/clangd.lua
+++ nvim/lsp/clangd.lua
@@ -2,6 +2,6 @@ return {
     cmd = { "clangd", "--background-index", "--clang-tidy", "--log=verbose" },
     filetypes = { "c", "h"},
     init_options = {
-        fallbackFlags = { '-std=c2x' },
+        fallbackFlags = { '-std=c2x', '-Wall' },
     },
 }
